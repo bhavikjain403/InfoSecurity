01bf11857526ff77f93c88e71131678d6f2fb5b5
Bhavik Jain <bhavikjain403@gmail.com>
2022-12-18 17:50:58 +0530

    Caesar cipher

---
+++ Q1.py
@@ -0,0 +1,24 @@
+# Encyrption
+
+s=input("Enter plain text : ")
+k=int(input("Enter key : "))
+ans=""
+for i in s:
+    if i==" ":
+        ans+="0"
+    else:
+        ans+=chr((ord(i)+k-97)%26+97)
+print("Encrypted text is :",ans)
+
+
+# Decryption
+
+s=input("Enter encrypted text : ")
+k=int(input("Enter key : "))
+ans=""
+for i in s:
+    if i==" ":
+        ans+="0"
+    else:
+        ans+=chr((ord(i)-k-97+26)%26+97)
+print("Plain text is :",ans)
